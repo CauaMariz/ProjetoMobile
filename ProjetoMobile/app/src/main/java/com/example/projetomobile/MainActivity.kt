e0dc2194c9dd6dc681564e7a2a137e9d90b38b0e
Caua Mariz DEV <marizcaua88@gmail.com>
2025-04-03 15:03:02 -0300

    Update MainActivity.kt

--- ProjetoMobile/app/src/main/java/com/example/projetomobile/MainActivity.kt
+++ ProjetoMobile/app/src/main/java/com/example/projetomobile/MainActivity.kt
@@ -1,6 +1,8 @@
-package com.example.projetomobile
+package com.example.teste
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun startProgram(view: View) {
+        val intent = Intent(this,HomeScreen::class.java);
+        startActivity(intent);
     }
 }
